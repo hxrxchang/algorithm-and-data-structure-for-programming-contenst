6eda67c928ac5c18bd5e2495dd69e78a504dd8d2
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-11-17 12:57:27 +0900

    insertion sort

---
+++ src/3rd-chapter/insertion-sort/insertion-sort.test.ts
@@ -0,0 +1,9 @@
+import { insertionSort } from "./insertion-sort";
+
+it("insertion concept 1", () => {
+  expect(insertionSort([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+});
+
+it("insertion concept 2", () => {
+  expect(insertionSort([3, 2, 1, 4])).toEqual([1, 2, 3, 4]);
+});
---
+++ src/3rd-chapter/insertion-sort/insertion-sort.ts
@@ -0,0 +1,13 @@
+export function insertionSort(array: number[]): number[] {
+  array = array.slice();
+  for (let i = 1; i < array.length; i++) {
+    const current = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
